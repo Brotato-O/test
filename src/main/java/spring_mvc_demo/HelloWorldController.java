b1ca50a2dfc64bb037dfe65fd623f97fad2f79cf
KaiSKTT1 <phucnguyen17072005@gmail.com>
2025-06-15 00:36:45 +0700

    Annotations: RequestParams and RequestMappings

--- src/main/java/spring_mvc_demo/HelloWorldController.java
+++ src/main/java/spring_mvc_demo/HelloWorldController.java
@@ -5,8 +5,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
 	//need a controller method to show the initial HTML Form
@@ -29,6 +31,18 @@ public class HelloWorldController {
 
 		//read the request parameter form the HTML form
 		String theName = request.getParameter("studentName");
+		//convert the data to all caps
+		theName = theName.toUpperCase();
+		//create the message
+		String res = "Yo! " + theName;
+		//add message to the model
+		model.addAttribute("message", res); // Lỗi đã được sửa ở đây: addAddtribute -> addAttribute
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+
 		//convert the data to all caps
 		theName = theName.toUpperCase();
 		//create the message
