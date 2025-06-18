d15707e40ad0e6aa145129c41a014ab9ce2f000a
kaiST <phucnguyen17072005@gmail.com>
2025-06-19 01:10:10 +0700

    hibernate cfg.xml and Annotation: @Entity, @Tabel, @ID, @Column

---
+++ src/main/java/hibernate_test/Test_JDBC.java
@@ -0,0 +1,24 @@
+package hibernate_test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Test_JDBC {
+	
+	public static void main(String[] args) {
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		String user = "hbstudent";
+		String pass = "hbstudent";
+		
+		try {
+			System.out.println("Connecting to database: " + jdbcUrl);
+			
+			Connection myConn = DriverManager.getConnection(jdbcUrl,user,pass);
+			System.out.println("Connection succesful!!!");
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
